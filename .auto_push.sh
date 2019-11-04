8574e87849da6e6c807b1799acffa505ca1444f9
Max Escher <mescher@umd.edu>
2019-11-04 10:35:31 -0500

    Add pseduo code

---
+++ .auto_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pull a branch, update local files accordingly, and push changes to same branch on remote
+
+# git check out - check out the desired branch (usueally the gh-pages branch)
+# git pull origin or 'git up' to update local with any possible remote changes.
+# remove/add/update local files
+# add/commit local changes in new commit.
+# git push origin
