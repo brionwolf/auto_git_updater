f6e066f78226b81848dceb9344246f57b031e576
Max Escher <mescher@umd.edu>
2019-11-04 12:48:49 -0500

    Add git remote update, Add git merge scripts

--- .auto_push.sh
+++ .auto_push.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-# Global Arguments
+# 0) Pull a branch, update local files accordingly, and push changes to same branch on remote
+# 0.a) Global Arguments
 which_branch=$1
 
-# Pull a branch, update local files accordingly, and push changes to same branch on remote
-
-# git check out - check out the desired branch (usueally the gh-pages branch)
-
-# get the current branch
+# -------------------------------
+# 1) git check out - check out the desired branch (usueally the gh-pages branch)
+# 1.a) get the current branch
 current_branch=$(git branch | grep \* | cut -d ' ' -f2)
 
-# Switch to branch passed in as an argument.
+# 1.b) Switch to branch passed in as an argument.
 if [ $# -eq 0  ]
   then
-    echo "No branch provided, using current branch: $current_branch"
+    echo -e "No branch provided, using current branch '$current_branch'."
   else
     env -i git checkout $which_branch
 fi
 
-# git pull origin or 'git up' to update local with any possible remote changes.
-# remove/add/update local files
-# add/commit local changes in new commit.
-# git push origin
+# -------------------------------
+# 2) git pull origin or 'git up' to update local with any possible remote changes.
+env -i git remote update -p
+env -i git merge --ff-only @{u}
+
+# 2.a) Why use `remote update`, and `git merge` over `git pull`: https://stackoverflow.com/questions/15316601/in-what-cases-could-git-pull-be-harmful/15316602#15316602
+
+# -------------------------------
+# 3. remove/add/update local files
+
+# -------------------------------
+# 4. add/commit local changes in new commit.
+
+# -------------------------------
+# 5. git push origin
+
+# -------------------------------
+
