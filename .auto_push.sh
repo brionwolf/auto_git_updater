69affadf20fe77783a24057506631b93f0d7704d
Max Escher <mescher@umd.edu>
2019-11-04 10:59:32 -0500

    Add Checkout script

--- .auto_push.sh
+++ .auto_push.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Global Arguments
+which_branch=$1
+
 # Pull a branch, update local files accordingly, and push changes to same branch on remote
 
 # git check out - check out the desired branch (usueally the gh-pages branch)
+
+# get the current branch
+current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+
+# Switch to branch passed in as an argument.
+if [ $# -eq 0  ]
+  then
+    echo "No branch provided, using current branch: $current_branch"
+  else
+    env -i git checkout $which_branch
+fi
+
 # git pull origin or 'git up' to update local with any possible remote changes.
 # remove/add/update local files
 # add/commit local changes in new commit.
