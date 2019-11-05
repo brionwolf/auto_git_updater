581fa0296ffe65d482620216e91c390c7c29a816
Max Escher <mescher@umd.edu>
2019-11-05 10:33:08 -0500

    Add check for dirty state

--- .auto_push.sh
+++ .auto_push.sh
@@ -17,9 +17,10 @@
 # ----------------------------------
 which_branch=$1
 
-# -------------------------------
+# ----------------------------------
 # 1) Checkout the desired branch or create a new one
 current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+
 if [[ $# -eq 0 ]]
   then
     echo "No branch provided, using current branch: $current_branch"
@@ -42,24 +43,30 @@ if [[ $# -eq 0 ]]
     fi
 fi
 
-# -------------------------------
+# ----------------------------------
 # 2) Make sure the branch is up to date with "git pull"
   # Note: Why use `remote update`, and `git merge` over `git pull`: https://stackoverflow.com/questions/15316601/in-what-cases-could-git-pull-be-harmful/15316602#15316602
 env -i git remote update -p
 env -i git merge --ff-only @{u}
 
-# -------------------------------
+# ----------------------------------
 # 3) Run an executable that makes local changes
 
-# -------------------------------
+# ----------------------------------
 # 4) Commit changes with a message including a datetime Stamp
 echo "Current git status"
 env -i git status
-env -i git add $file_to_create_or_edit
-git commit -m "File updated with current date - $(date)"
 
-# -------------------------------
+if git diff-index --quiet HEAD --;
+  then
+    echo "No changes were made"
+  else
+    env -i git add $file_to_create_or_edit
+    git commit -m "File updated with current date - $(date)"
+fi
+
+# ----------------------------------
 # 5) Push local changes to the remote instance of the curren branch
 git push origin $(git branch | grep \* | cut -d ' ' -f2)
 
-# -------------------------------
+# ----------------------------------
